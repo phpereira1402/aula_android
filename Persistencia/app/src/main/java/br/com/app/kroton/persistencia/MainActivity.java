5bd860e2907365b11a3cd7394931c502d9964ce3
Paulo Henrique Pereira <phpereira1402@gmail.com>
2020-11-01 19:08:12 -0300

    2nd commit

--- Persistencia/app/src/main/java/br/com/app/kroton/persistencia/MainActivity.java
+++ Persistencia/app/src/main/java/br/com/app/kroton/persistencia/MainActivity.java
@@ -1,8 +1,13 @@
 package br.com.app.kroton.persistencia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,6 +31,16 @@ public class MainActivity extends AppCompatActivity {
     Button btCarregar;
     Button btGravar;
 
+    Button btCarregarMemoriaExterna;
+    Button btGravarMemoriaExterna;
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
         etNome = (EditText) findViewById(R.id.etNome);
         btCarregar = (Button) findViewById(R.id.btCarregar);
         btGravar = (Button) findViewById(R.id.btGravar);
+        btCarregarMemoriaExterna = (Button) findViewById(R.id.btCarregarMemoriaExterna);
+        btGravarMemoriaExterna = (Button) findViewById(R.id.btGravarMemoriaExterna);
 
         btCarregar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,11 +71,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btCarregarMemoriaExterna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Pessoa p = LerMemoriaExterna();
+                etId.setText(String.valueOf(p.id));
+                etNome.setText(p.nome);
+            }
+        });
 
+        btGravarMemoriaExterna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Pessoa p = new Pessoa(Integer.parseInt(etId.getText().toString()), etNome.getText().toString());
+                GravarMemoriaExterna(p);
+            }
+        });
 
-        Pessoa p = new Pessoa(1, "Aluno");
-
-        GravarMemoriaInterna(p);
+        verifyStoragePermissions(this);
 
     }
 
@@ -72,7 +102,6 @@ public class MainActivity extends AppCompatActivity {
             fis.close();
             ois.close();
         } catch (Exception ex){
-            Toast.makeText(this, "teste", Toast.LENGTH_SHORT).show();
 
         }
 
@@ -97,6 +126,71 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private boolean GravarMemoriaExterna(Pessoa pessoa){
+
+
+
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)){
+            //File diretorio = new File(Environment.getExternalStorageDirectory() + "/Documents");
+            File diretorio = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+
+            File arquivo = new File(diretorio, "pessoa.obj");
+
+
+
+            try {
+
+                if (!diretorio.exists())
+                    diretorio.mkdirs();
+
+                if (!arquivo.exists())
+                    arquivo.createNewFile();
+
+                FileOutputStream fos = new FileOutputStream(arquivo);
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+                oos.writeObject(pessoa);
+                fos.close();
+                oos.close();
+            } catch (Exception ex){
+                return false;
+            }
+
+            return true;
+        }
+        return false;
+    }
+
+    private Pessoa LerMemoriaExterna(){
+        String state = Environment.getExternalStorageState();
+        Pessoa p = null;
+        if (Environment.MEDIA_MOUNTED.equals(state)){
+            File diretorio = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+            File arquivo = new File(diretorio, "pessoa.obj");
+
+
+            try {
+                if (!diretorio.exists())
+                    diretorio.mkdirs();
+
+                if (!arquivo.exists())
+                    arquivo.createNewFile();
+
+                FileInputStream fis = new FileInputStream(arquivo);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                p = (Pessoa) ois.readObject();
+                fis.close();
+                ois.close();
+            } catch (Exception ex){
+
+            }
+        }
+        return p;
+    }
+
+
+
+
     public static class Pessoa implements Serializable {
 
         public int id;
@@ -105,6 +199,20 @@ public class MainActivity extends AppCompatActivity {
         Pessoa(int id, String nome){
             this.id = id;
             this.nome = nome;
+        }
+    }
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
         }
     }
 
